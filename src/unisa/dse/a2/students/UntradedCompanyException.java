1006cc96fc534157d2612ce247975bf6087bb1f8
BonnorakSUN <sunby008@mymail.unisa.edu.au>
2025-05-13 23:24:30 +0930

    Update UntradedCompanyException.java
    update UntradedCompany Class

--- src/unisa/dse/a2/students/UntradedCompanyException.java
+++ src/unisa/dse/a2/students/UntradedCompanyException.java
@@ -1,8 +1,10 @@
 package unisa.dse.a2.students;
 
-public class UntradedCompanyException
-{
-	public UntradedCompanyException(String companyCode)
-	{
+import java.lang.Exception;
+
+public class UntradedCompanyException extends Exception{
+	
+	public UntradedCompanyException(String companyCode){
+		super(companyCode + "is not a listed company on this exchange");
 	}
 }
